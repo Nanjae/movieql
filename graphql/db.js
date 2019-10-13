944b057b55bffa0accee8b659112d3dcef7216a4
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-10-14 00:42:15 +0900

    #13 Wrapping a REST API with GraphQL

---
+++ graphql/db.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+  let REQUST_URL = API_URL;
+  if (limit > 0) {
+    REQUST_URL += `&limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
